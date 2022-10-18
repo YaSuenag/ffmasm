5ac21e1c933b84f4c931371f21b565ef43db5af0
Yasumasa Suenaga <yasuenag@gmail.com>
2022-10-18 22:05:01 +0900

    Add benchmark

---
+++ benchmark/README.md
@@ -0,0 +1,34 @@
+Benchmark for ffmasm
+===================
+
+[JMH](https://github.com/openjdk/jmh) benchmark to comparison between JNI and ffmasm.
+
+This benchmark runs RDTSC instruction. JNI function is written in assembly code in [rdrand.S](src/main/native/rdrand.S), and same code is assembled by ffmasm in [FFMComparison.java](src/main/java/com/yasuenag/ffmasm/benchmark/FFMComparison.java). So you can see calling performacne JNI and Foreign Function & Memory API.
+
+# Requirements
+
+* Java 19
+* Maven
+* GNU Make
+* GNU assembler
+
+# How to build
+
+```sh
+$ cd /path/to/ffasm
+$ mvn install
+$ cd benchmark
+$ mvn package
+```
+
+# Check result from RDTSC
+
+```sh
+$ mvn exec:exec@single-run
+```
+
+# Run benchmark
+
+```sh
+$ $JAVA_HOME/bin/java -jar target/ffmasm-benchmark-1.0.0.jar
+```
---
+++ benchmark/src/main/java/com/yasuenag/ffmasm/benchmark/FFMComparison.java
@@ -0,0 +1,79 @@
+package com.yasuenag.ffmasm.benchmark;
+
+import java.lang.foreign.*;
+import java.lang.invoke.*;
+import java.util.*;
+import java.util.concurrent.*;
+
+import com.yasuenag.ffmasm.*;
+import com.yasuenag.ffmasm.amd64.*;
+
+import org.openjdk.jmh.annotations.*;
+
+
+@State(Scope.Benchmark)
+@BenchmarkMode(Mode.Throughput)
+@Fork(value = 1, jvmArgsAppend = {"--enable-preview", "--enable-native-access=ALL-UNNAMED", "-Djava.library.path=.", "-Xms4g", "-Xmx4g", "-XX:+UnlockExperimentalVMOptions", "-XX:+UseEpsilonGC", "-XX:+AlwaysPreTouch"})
+@Warmup(iterations = 1, time = 3, timeUnit = TimeUnit.SECONDS)
+@Measurement(iterations = 3, time = 10, timeUnit = TimeUnit.SECONDS)
+public class FFMComparison{
+
+  private CodeSegment seg;
+
+  private MethodHandle ffmRDTSC;
+
+  @Setup
+  public void setup(){
+    System.loadLibrary("rdtsc");
+
+    try{
+      seg = new CodeSegment();
+      ffmRDTSC = AMD64AsmBuilder.create(seg, FunctionDescriptor.of(ValueLayout.JAVA_LONG))
+           /* push %rbp      */ .push(Register.RBP)
+           /* mov %rsp, %rbp */ .movMR(Register.RSP, Register.RBP, OptionalInt.empty())
+           /* rdtsc          */ .rdtsc()
+           /* shl $32, %rdx  */ .shl(Register.RDX, (byte)32, OptionalInt.empty())
+           /* or %rdx, %rax  */ .orMR(Register.RDX, Register.RAX, OptionalInt.empty())
+           /* leave          */ .leave()
+           /* ret            */ .ret()
+                                .build();
+    }
+    catch(PlatformException | UnsupportedPlatformException e){
+      throw new RuntimeException(e);
+    }
+  }
+
+  @Benchmark
+  public native long rdtsc();
+
+  @Benchmark
+  public long invokeFFMRDTSC(){
+    try{
+      return (long)ffmRDTSC.invoke();
+    }
+    catch(Throwable t){
+      throw new RuntimeException(t);
+    }
+  }
+
+  @TearDown
+  public void tearDown(){
+    try{
+      seg.close();
+    }
+    catch(Exception e){
+      throw new RuntimeException(e);
+    }
+  }
+
+  public static void main(String[] args){
+    var inst = new FFMComparison();
+    inst.setup();
+    long nativeVal = inst.rdtsc();
+    long ffmVal = inst.invokeFFMRDTSC();
+
+    System.out.println("native: " + nativeVal);
+    System.out.println("   FFM: " + ffmVal);
+  }
+
+}
