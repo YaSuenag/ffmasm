9a4dd5bd680377bec06d2fd5b2e13dd196fe0a4d
Yasumasa Suenaga <yasuenag@gmail.com>
2024-04-19 23:13:58 +0900

    [REFACTORING] callback function for NativeRegister

--- src/main/java/com/yasuenag/ffmasm/internal/amd64/AMD64NativeRegister.java
+++ src/main/java/com/yasuenag/ffmasm/internal/amd64/AMD64NativeRegister.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2023, Yasumasa Suenaga
+ * Copyright (C) 2023, 2024, Yasumasa Suenaga
  *
  * This file is part of ffmasm.
  *
@@ -62,42 +62,63 @@ public final class AMD64NativeRegister extends NativeRegister{
     var jvmtiEnv = JvmtiEnv.getInstance();
     var regs = CallingRegisters.getRegs();
     var desc = FunctionDescriptor.ofVoid(ValueLayout.ADDRESS); // callbackParam
+
+    /*             S T A C K   A L L O C A T I O N
+         -------------------------------------------------  ---- RSP (RBP-64)
+        | Shadow space for arg4                           |
+        |-------------------------------------------------|
+        | Shadow space for arg3                           |
+        |-------------------------------------------------|
+        | Shadow space for arg2                           |
+        |-------------------------------------------------|
+        | Shadow space for arg1                           |
+        |-------------------------------------------------|
+        | Stack alignment                                 |
+        |-------------------------------------------------|
+        | Number of jclasses (from GetLoadedClasses())    |
+        |-------------------------------------------------|
+        | Pointer to jclass array from GetLoadedClasses() |
+        |-------------------------------------------------|
+        | Saved Register 1                                |
+        |-------------------------------------------------| ---- RBP
+        | Previous RBP                                    |
+        |-------------------------------------------------|
+        | Return Address                                  |
+         -------------------------------------------------
+     */
     registerStub = AMD64AsmBuilder.create(AMD64AsmBuilder.class, seg, desc)
-      // prologue
-        /* push %rbp           */ .push(Register.RBP)
-        /* mov %rsp, %rbp      */ .movMR(Register.RSP, Register.RBP, OptionalInt.empty())
-      // evacutate
-        /* push savedReg1      */ .push(regs.savedReg1())
-        /* mov arg1, savedReg1 */ .movMR(regs.arg1(), regs.savedReg1(), OptionalInt.empty()) // callbackParam
-      // call GetLoadedClasses()
-        /* sub $16, %rsp       */ .sub(Register.RSP, 16, OptionalInt.empty())
-        /* lea 8(%rsp), arg3   */ .lea(regs.arg3(), Register.RSP, 8) // classes
-        /* mov %rsp, arg2      */ .movMR(Register.RSP, regs.arg2(), OptionalInt.empty()) // count
-        /* mov addr, arg1      */ .movImm(regs.arg1(), jvmtiEnv.getRawAddress()) // address of jvmtiEnv
-        /* mov addr, tmpReg1   */ .movImm(regs.tmpReg1(), jvmtiEnv.getLoadedClassesAddr().address()) // address of GetLoadedClasses()
-        /* sub $40, %rsp       */ .sub(Register.RSP, 40, OptionalInt.empty()) // Shadow stack (for Windows: 32 bytes) + stack alignment (8 bytes)
-        /* call tmpReg1        */ .call(regs.tmpReg1())
-        /* add $40, %rsp       */ .add(Register.RSP, 40, OptionalInt.empty()) // Recover shadow stack + stack alignments
-      // call callback(jclass *classes, jint class_count)
-        /* pop arg2            */ .pop(regs.arg2(), OptionalInt.empty())
-        /* mov (%rsp), arg1    */ .movRM(regs.arg1(), Register.RSP, OptionalInt.of(0))
-        /* mov returnReg, arg3 */ .movMR(regs.returnReg(), regs.arg3(), OptionalInt.empty()) // result of GetLoadedClasses()
-        /* mov savedReg1, arg4 */ .movMR(regs.savedReg1(), regs.arg4(), OptionalInt.empty()) // callbackParam
-        /* mov addr, tmpReg1   */ .movImm(regs.tmpReg1(), cbStub.address()) // address of callback
-        /* sub $32, %rsp       */ .sub(Register.RSP, 32, OptionalInt.empty()) // Shadow stack (for Windows: 32 bytes)
-        /* call tmpReg1        */ .call(regs.tmpReg1())
-        /* add $32, %rsp       */ .add(Register.RSP, 32, OptionalInt.empty()) // Recover shadow stack
-      // call Deallocate()
-        /* mov addr, arg1      */ .movImm(regs.arg1(), jvmtiEnv.getRawAddress()) // address of jvmtiEnv
-        /* pop arg2            */ .pop(regs.arg2(), OptionalInt.empty()) // classes
-        /* mov addr, tmpReg1   */ .movImm(regs.tmpReg1(), jvmtiEnv.deallocateAddr().address()) // address of Deallocate()
-        /* sub $40, %rsp       */ .sub(Register.RSP, 40, OptionalInt.empty()) // Shadow stack (for Windows: 32 bytes) + stack alignment (8 bytes)
-        /* call tmpReg1        */ .call(regs.tmpReg1())
-        /* add $40, %rsp       */ .add(Register.RSP, 40, OptionalInt.empty()) // Recover shadow stack + stack alignments
-      // epilogue
-        /* pop savedReg1       */ .pop(regs.savedReg1(), OptionalInt.empty())
-        /* leave               */ .leave()
-        /* ret                 */ .ret()
+  // prologue
+    /* push %rbp               */ .push(Register.RBP)
+    /* mov %rsp, %rbp          */ .movMR(Register.RSP, Register.RBP, OptionalInt.empty())
+    /* sub $64, %rsp           */ .sub(Register.RSP, 64, OptionalInt.empty())
+    /* mov savedReg1, -8(%rbp) */ .movMR(regs.savedReg1(), Register.RBP, OptionalInt.of(-8))
+    /* mov arg1, savedReg1     */ .movMR(regs.arg1(), regs.savedReg1(), OptionalInt.empty())
+
+  // call GetLoadedClasses()
+    /* lea -24(%rbp), arg2     */ .lea(regs.arg2(), Register.RBP, -24) // count
+    /* lea -16(%rbp), arg3     */ .lea(regs.arg3(), Register.RBP, -16)  // classes
+    /* mov addr, arg1          */ .movImm(regs.arg1(), jvmtiEnv.getRawAddress()) // address of jvmtiEnv
+    /* mov addr, tmpReg1       */ .movImm(regs.tmpReg1(), jvmtiEnv.getLoadedClassesAddr().address()) // address of GetLoadedClasses()
+    /* call tmpReg1            */ .call(regs.tmpReg1())
+
+  // call callback(jclass *classes, jint class_count)
+    /* mov -24(%rbp), arg2     */ .movRM(regs.arg2(), Register.RBP, OptionalInt.of(-24)) // count
+    /* mov -16(%rbp), arg1     */ .movRM(regs.arg1(), Register.RBP, OptionalInt.of(-16))  // classes
+    /* mov returnReg, arg3     */ .movMR(regs.returnReg(), regs.arg3(), OptionalInt.empty()) // result of GetLoadedClasses()
+    /* mov savedReg1, arg4     */ .movMR(regs.savedReg1(), regs.arg4(), OptionalInt.empty()) // callbackParam
+    /* mov addr, tmpReg1       */ .movImm(regs.tmpReg1(), cbStub.address()) // address of callback
+    /* call tmpReg1            */ .call(regs.tmpReg1())
+
+  // call Deallocate()
+    /* mov addr, arg1          */ .movImm(regs.arg1(), jvmtiEnv.getRawAddress()) // address of jvmtiEnv
+    /* mov -16(%rbp), arg1     */ .movRM(regs.arg1(), Register.RBP, OptionalInt.of(-16))  // classes
+    /* mov addr, tmpReg1       */ .movImm(regs.tmpReg1(), jvmtiEnv.deallocateAddr().address()) // address of Deallocate()
+    /* call tmpReg1            */ .call(regs.tmpReg1())
+
+  // epilogue
+    /* mov -8(%rbp), savedReg1 */ .movRM(regs.savedReg1(), Register.RBP, OptionalInt.of(-8))
+    /* leave                   */ .leave()
+    /* ret                     */ .ret()
                                   .build();
   }
 
